6ef591c8c752e9ebfa5a30d8b124b8161e916cf3
Dmitry Gavrilenko <dmitry.gavrilenko@icloud.com>
2017-10-16 21:47:41 +0300

    Add map loading

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,1 +1,31 @@
 #include "Map.h"
+#include <cstdio>
+#include <sstream>
+
+Map::Map(std::istream& is) {
+  std::string line;
+
+  while (std::getline(is, line)) {
+    std::istringstream iss(line);
+
+    Waypoint wp;
+
+    iss >> wp.pos_.x();
+    iss >> wp.pos_.y();
+    iss >> wp.s_;
+    iss >> wp.dir_.x();
+    iss >> wp.dir_.y();
+
+    track_.push_back(wp);
+  }
+
+  if (!track_.empty()) {
+    const double tail = (track_.back().pos_ - track_.front().pos_).norm();
+    length_ = track_.back().s_ + tail;
+  }
+}
+
+
+double Map::length() const {
+  return length_;
+}
--- src/Map.h
+++ src/Map.h
@@ -1,7 +1,19 @@
 #ifndef SRC_MAP_H_
 #define SRC_MAP_H_
 
+#include "Waypoint.h"
+#include <istream>
+#include <vector>
+
 class Map {
+ public:
+  explicit Map(std::istream& is);
+
+  double length() const;
+
+ private:
+  std::vector<Waypoint> track_;
+  double length_{0.0};
 };
 
 #endif  // SRC_MAP_H_
